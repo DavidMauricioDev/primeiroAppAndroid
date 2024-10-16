0f7a67167de74de4038a7d220471536fe1932fe2
David <david.mauricio-cont@hotmail.com>
2024-10-15 22:52:42 -0300

    PrimeiroAppAndroid

---
+++ app/src/main/java/com/davidmauricio/myapplication/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.davidmauricio.myapplication
+
+import android.os.Bundle
+import android.view.TextureView
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun sortear(view: View){
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random().nextInt(25)
+        textoResultado.setText("número gerado $numero")
+
+    }
+
+
+
+
+
+
+}
